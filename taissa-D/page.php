ff02faa2299233e91436ce0911d6c47c26f17e01
Taissa470 <fernandes.taissa@escola.pr.gov.br>
2025-09-15 11:37:31 -0300

    Add files via upload

--- taissa-D/page.php
+++ taissa-D/page.php
@@ -3,9 +3,15 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Formulario</title>
 </head>
 <body>
-    <h1>site</h1>
+    <h1>FORMULARIO</h1>
+    <div style="width: 250px; height: 250px; border: 2px solid rgb(62, 1, 1); padding: 10px; border-radius: 15px; ">
+        <form action="">
+      <input type="text" placeholder="login" id="login">
+      <input type="password" placeholder="senha" id="senha">
+      <input type="submit" onclick="logar(); return false">
+    </form>
 </body>
 </html>
